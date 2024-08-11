efa244bf563d8307fa452f13611313232bf1b797
Jules TIMMERMAN <jules.timmerman@ens-rennes.fr>
2024-08-11 17:12:50 +0200

    Added setAPIKey

--- gif.lua
+++ gif.lua
@@ -5,4 +5,11 @@ local https = require("ssl.https")
 local search_endpoint = "https://tenor.googleapis.com/v2/search?"
 local API_KEY = nil
 
+---Sets the global API key
+---Should be used before any other calls
+---@param apiKey string
+function M.setAPIKey(apiKey)
+	API_KEY = apiKey
+end
+
 return M
